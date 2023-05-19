ba99d260f3016399fe175b7da4b0f67ec6757da3
Elvismelook <elvismelook@gmail.com>
2023-05-18 23:15:02 -0300

    uptodo2

---
+++ script.js
@@ -0,0 +1,17 @@
+let nova = document.createElement ("div")
+let tarefa = document.getElementById('tarefa')
+var texto = tarefa.value
+nova.setAttribute("class", "divinova")
+
+
+    
+
+ 
+
+function criar(){
+    
+    + document.getElementById('geradas').appendChild(nova)
+    
+
+    
+}
